ff3294462e27d56e0409b4f15e133fdcf49f4c16
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-05-09 11:52:40 -0400

    adding install

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd station/functions
+npm install --save-dev firebase-functions-test
+npm install --save-dev mocha
+npm install --save-dev firebase-admin
+npm install --save-dev firebase-functions
+
